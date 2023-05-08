94dbe871f0e525d126cf5ce3128423e5010f895a
kubilaybee <kubilay.bekdemir@anymile.io>
2023-05-08 15:24:05 +0300

    Initial commit

---
+++ src/test/java/com/example/jpa/artiffactjpain10/ArtiffactJpaIn10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpa.artiffactjpain10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtiffactJpaIn10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
